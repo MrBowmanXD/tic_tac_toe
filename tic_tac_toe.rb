b469ddf83ba3ad799b62b5e1f9e8e0670429b5fd
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-04-04 16:08:31 +0100

    Added functionality to Game class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,13 +1,15 @@
 class Player
   @player_one_presence = false
+  attr_accessor :player_one
+  attr_accessor :player_two
 
   def initialize
     if @player_one_presence == false
-      puts "What is your name, player one?"
+      puts "\nWhat is your name, player one?\n"
       @player_one = gets.chomp
       @player_one_presence = true
     else
-      puts "What is your name, player one?"
+      puts "\nWhat is your name, player two?\n"
       @player_two = gets.chomp
     end
   end
@@ -16,7 +18,7 @@ end
 class Computer
 
   def initialize
-    puts "Oh, hello there.\nI came to entertain you.\nI am the computer.\nNice to meet you.\n\n"
+    puts "Oh, hello there.\n\nI came to entertain you.\n\nI am the computer.\n\nNice to meet you.\n\n"
   end
 
   private
@@ -30,22 +32,22 @@ class Computer
   end
 end
 
-class Greetings
+module Greetings
 
-  def initialize
+  def initial_response
     system("clear")
     puts "Welcome to my game of tic-tac-toe\n
     Please choose:\n
     s) Singleplayer\n
-    m) Multiplayer\n"
+    m) Multiplayer\n\n"
   end
 
   def player_one
-    puts "What is your name player one?"
+    puts "\nWhat is your name player one?\n"
   end
 
   def player_two
-    puts "What is your name player two?"
+    puts "\nWhat is your name player two?\n"
   end
   
 end
@@ -56,24 +58,31 @@ module GameSymbol
 end
 
 class Game
+  include Greetings
   include GameSymbol
   @@times_played = 0
 
-  attr_accessor :player_one
-  attr_accessor :player_two
+  attr_accessor :player_one_name
+  attr_accessor :player_two_name
+  attr_accessor :game_board_array
 
-  def initialize(game_mode = s)
+  def initialize
+    initial_response
+    @game_board_array = Array.new(9, ' ')
+  end
+
+  def start_game game_mode
     system("clear")
     @game_mode = game_mode
     @@times_played += 1
-    if game_mode == 's'
+    if @game_mode == 's'
       single_player
-    elsif game_mode == 'm'
+    elsif @game_mode == 'm'
       multi_player
     else
       system("clear")
       puts "You have to choose between a valid game mode"
-      Greetings.initialize
+      initial_response 
       new_choice = gets.chomp
       initialize new_choice
     end
@@ -83,23 +92,45 @@ class Game
   def single_player
     @computer = Computer.new
     @player_one = Player.new 
+    @player_one_name = @player_one.player_one
   end
 
   private 
-  def multi_player game_mode
-    if game_mode == m
-      player_one = Player.new
-      player_two = Player.new
+  def multi_player
+    @player_one = Player.new
+    @player_one_name = @player_one.player_one
+    @player_two = Player.new
+    @player_two_name = @player_two.player_two
+  end
+
+  private
+  def player_choice number, player
+    if number.to_i >= 0 && number.to_i < 9
+      @game_board_array[number.to_i] = 'X' if player == @player_one_name
+      @game_board_array[number.to_i] = 'O' if player == @player_two_name || player == 'Computer'
     end
   end
 
+  private
+  def check_win symbol
+     
+  end
+
   public
-  def game_board player = @player_one
+  def chosen_number number, player
+    player_choice number, player
+  end
+
+
+  public
+  def game_board player = @player_one, game_board_array = @game_board_array
     system("clear")
-    puts "|_|_|_|\n
-|_|_|_|\n
-|_|_|_|\n
-Please enter a valid number from one to nine to mark the spot #{player}"
+    puts "| #{@game_board_array[0]} | #{@game_board_array[1]} | #{@game_board_array[2]} |\n"
+    puts "-------------\n"
+    puts "| #{@game_board_array[3]} | #{@game_board_array[4]} | #{@game_board_array[5]} |\n"
+    puts "-------------\n"
+    puts "| #{@game_board_array[6]} | #{@game_board_array[7]} | #{@game_board_array[8]} |\n\n"
+    puts "Please enter a valid number from zero to eight to mark the spot #{player}"
   end
 
   def start 
@@ -112,8 +143,13 @@ Please enter a valid number from one to nine to mark the spot #{player}"
   end
 end
 
-
-
-message = Greetings.new
+round_one = Game.new 
 game_mode = gets.chomp
-round_one = Game.new game_mode
+round_one.start_game game_mode
+round_one.game_board round_one.player_one_name, round_one.game_board_array
+x_marks_the_spot = gets.chomp
+round_one.chosen_number x_marks_the_spot, round_one.player_one_name
+round_one.game_board round_one.player_two_name, round_one.game_board_array
+o_marks_the_spot = gets.chomp
+round_one.chosen_number o_marks_the_spot, round_one.player_two_name
+round_one.game_board round_one.player_one_name, round_one.game_board_array
